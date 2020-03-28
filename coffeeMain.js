261d8989dfd58d8744ddc80a149aeaf896657a10
j0c3 <alvarezjclyn@gmail.com>
2020-03-27 22:49:00 -0700

    added chart

--- coffeeMain.js
+++ coffeeMain.js
@@ -1,3 +1,4 @@
+
 var display = document.getElementById('resultP');
 var displayDrink = document.getElementById('displayDrink');
 var displayAmounts = document.getElementById('displayAmounts');
@@ -14,14 +15,15 @@ function calculateBedTime() {
     var bedTime=Math.round((6*Math.log(indexValue/45))/(Math.log(2)));
 
 
-    //output new bedtime
+    //output new bedtime and fill chart
+    fillChart(currentTime.getHours(),currentTime.getMinutes(),indexValue,bedTime);
     currentTime.setHours(currentTime.getHours() + bedTime);
 
     if (currentTime.getHours() > 11) {
         document.getElementById("ampm").innerText = "pm";
         currentTime.setHours(currentTime.getHours()-12);
     } else {
-        if(currentTime.getHours()==0) currentTime.setHours(12);
+        if(currentTime.getHours()===0) currentTime.setHours(12);
         document.getElementById("ampm").innerText = "am";
     }
     document.getElementById("bedtime").innerText = currentTime.getHours().toString() + ":" + String(currentTime.getMinutes()).padStart(2,"0");
@@ -54,10 +56,72 @@ function calculateC(){
     calculateBedTime();
 }
 
+function finalAmount(prevDose,eTime){
+    return prevDose/Math.pow(2,eTime/6);
+}
+var tTable = [], dTable=[];
+function fillChart(hours, minutes, intake, bedtime) {
+    //fill out chart
+    let isPM= false;
+    if(hours >12){
+        isPM=true;
+        hours=hours-12;
+    }
+    let dose = intake;
+
+    let i = 0;
+    for(;i<bedtime+4;i++) {
+
+        tTable.push(hours);
+        dTable.push(finalAmount(dose,i));
+        hours++;
+    }
+    // alert(tTable.toString());
+    drawChart(tTable);
+}
+// load frozen version 44
+google.charts.load('44', {
+    callback: function (){drawChart(tTable)},
+    packages: ['corechart']
+});
+
+// function drawChart(tTable)
+// {
+//
+// }
+function drawChart(tTable){
+    // create DataTable
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Time');
+    // data.addColumn('number', 'Id');
+    data.addColumn('number', 'mg');
+
+    // load data
+
+    for (var i = 0; i < tTable.length; i++) {
+        var row = [tTable[i], dTable[i]];
+        data.addRow(row);
+    }
+
+    var options = {
+        hAxis: {
+            title:'Time'
+        },
+        vAxis:{
+            title: "Caffeine left in body",
+            // scaleType: tTable
+        },
+    };
+
+    var chart = new  google.visualization.LineChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+
 function underAge(){
     document.getElementById("nope").innerHTML = "sorry you're too young to use this";
 }
-var clicks = 0;
+let clicks = 0;
 function cupCounter(){
 
     clicks ++;
